d3850ee5e84c359adc3035d84f6224e5feab927e
Yulia Gurevich <yulia.v.gurevich@gmail.com>
2019-09-26 23:04:17 +0300

    Add a link from the About page to the Quiz page.

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const about = () => {
+    const quizLink = <Link to="/quizes">Take the quiz</Link>
+
     return (
-        <p>Let me tell you about...</p>
+        <>
+            <h1>About the quiz</h1>
+            <p>Let me tell you about our quiz... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id blanditiis nam magni commodi natus velit fugiat soluta libero facere praesentium doloremque, deserunt nostrum eos inventore nihil quisquam eius, eligendi non! {quizLink}</p>
+        </>
     );
 }
 
