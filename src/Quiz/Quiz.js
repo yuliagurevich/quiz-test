90bc915fe2ab9db92d7a779579a0b6008b5cf257
Yulia Gurevich <yulia.v.gurevich@gmail.com>
2019-09-28 10:40:35 +0300

    Fixed incorrect condition for rendering radio buttons and checkboxes.

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -6,46 +6,46 @@ import LabeledCheckboxInput from "../LabeledCheckboxInput/LabeledCheckboxInput";
 import "./style.css";
 
 const quiz = props => {
-  const { quiz, onInputChange, currentValue } = props;
+    const { quiz, currentValue, onInputChange } = props;
 
-  let answers;
+    let answers;
 
-  if (quiz.correctAnswers.length === 1) {
-    answers = renderCheckboxes(quiz, currentValue, onInputChange);
-  } else {
-    answers = renderRadioButtons(quiz, currentValue, onInputChange);
-  }
+    if (quiz.correctAnswers.length !== 1) {
+        answers = renderCheckboxes(quiz, currentValue, onInputChange);
+    } else {
+        answers = renderRadioButtons(quiz, currentValue, onInputChange);
+    }
 
-  return (
-    <>
-      <p className="question">{quiz.question}</p>
-      <div className="radio-group">
-        {answers}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <p className="question">{quiz.question}</p>
+            <div className="radio-group">
+                {answers}
+            </div>
+        </>
+    );
 };
 
 const renderCheckboxes = (quiz, currentValue, onInputChange) => {
-  return quiz.answers.map(answer => <LabeledCheckboxInput
-    key={answer}
-    id={answer}
-    name={quiz.question}
-    value={answer}
-    currentValue={currentValue}
-    onChange={onInputChange}
-  />);
+    return quiz.answers.map(answer => <LabeledCheckboxInput
+        key={answer}
+        id={answer}
+        name={quiz.question}
+        value={answer}
+        currentValue={currentValue}
+        onChange={onInputChange}
+    />);
 }
 
 const renderRadioButtons = (quiz, currentValue, onInputChange) => {
-  return quiz.answers.map(answer => <LabeledRadioInput
-    key={answer}
-    id={answer}
-    name={quiz.question}
-    value={answer}
-    currentValue={currentValue}
-    onChange={onInputChange}
-  />);
+    return quiz.answers.map(answer => <LabeledRadioInput
+        key={answer}
+        id={answer}
+        name={quiz.question}
+        value={answer}
+        currentValue={currentValue}
+        onChange={onInputChange}
+    />);
 }
 
 export default quiz;
